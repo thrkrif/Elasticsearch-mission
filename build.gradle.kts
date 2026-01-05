0dba222b45b86bae59dca286e71f7a2cc4133a04
thrkrif <103479629+thrkrif@users.noreply.github.com>
2026-01-05 15:20:34 +0900

    Docker 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-data-elasticsearch-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
+    runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
     // Lombok
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
